286d943caf67588641b087efe4ffeee28087961f
曹力升 <c_youxin@163.com>
2017-05-23 14:19:49 +0800

    CHANGE server service path

--- b.sh
+++ b.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ng build --env=prod -base /server_management/ \
+ng build --env=prod -base /server/ \
     && rm -rf ./docs/ \
     && cp -r ./dist/ ./docs/
 #    && git checkout -- docs/CNAME \
--- src/api/api.const.ts
+++ src/api/api.const.ts
@@ -3,27 +3,27 @@ export class API {
   static mode: string;
   static api = {
     "server/list": {
-      "prod": "/server_management/server/list",
-      "dev": "http://localhost:8080/server_management/server/list"
+      "prod": "/server/server/list",
+      "dev": "http://localhost:8080/server/server/list"
     },
     "server/manipulation": {
       "prod": function (aDo, at) {
-        return `/server_management/server/manipulation?do=${aDo}&at=${at}`;
+        return `/server/server/manipulation?do=${aDo}&at=${at}`;
       },
       "dev": function (aDo, at) {
-        return `http://localhost:8080/server_management/server/manipulation?do=${aDo}&at=${at}`;
+        return `http://localhost:8080/server/server/manipulation?do=${aDo}&at=${at}`;
       }
     },
     "server/config": {
-      "prod": "/server_management/server/config",
-      "dev": "http://localhost:8080/server_management/server/config"
+      "prod": "/server/server/config",
+      "dev": "http://localhost:8080/server/server/config"
     },
     "server/config/get": {
       "prod": function (serverName) {
-        return `/server_management/server/config/get?serverName=${serverName}`;
+        return `/server/server/config/get?serverName=${serverName}`;
       },
       "dev": function (serverName) {
-        return `http://localhost:8080/server_management/server/config/get?serverName=${serverName}`;
+        return `http://localhost:8080/server/server/config/get?serverName=${serverName}`;
       }
     }
   };
