56057c6321a6ffa0464734c8db11db5886ac1a77
曹力升 <c_youxin@163.com>
2017-05-30 08:27:44 +0800

    nginx route change

--- b.sh
+++ b.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ng build --env=prod -base /server/ \
+ng build --env=prod -base /servers/ \
     && rm -rf ./docs/ \
     && cp -r ./dist/ ./docs/
 #    && git checkout -- docs/CNAME \
--- docs/main.bundle.js
+++ docs/main.bundle.js
@@ -77,27 +77,27 @@ var API = (function () {
     };
     API.api = {
         "server/list": {
-            "prod": "/server/server/list",
-            "dev": "http://localhost:8080/server/server/list"
+            "prod": "/servers/server/list",
+            "dev": "http://localhost:8080/servers/server/list"
         },
         "server/manipulation": {
             "prod": function (aDo, at) {
-                return "/server/server/manipulation?do=" + aDo + "&at=" + at;
+                return "/servers/server/manipulation?do=" + aDo + "&at=" + at;
             },
             "dev": function (aDo, at) {
-                return "http://localhost:8080/server/server/manipulation?do=" + aDo + "&at=" + at;
+                return "http://localhost:8080/servers/server/manipulation?do=" + aDo + "&at=" + at;
             }
         },
         "server/config": {
-            "prod": "/server/server/config",
-            "dev": "http://localhost:8080/server/server/config"
+            "prod": "/servers/server/config",
+            "dev": "http://localhost:8080/servers/server/config"
         },
         "server/config/get": {
             "prod": function (serverName) {
-                return "/server/server/config/get?serverName=" + serverName;
+                return "/servers/server/config/get?serverName=" + serverName;
             },
             "dev": function (serverName) {
-                return "http://localhost:8080/server/server/config/get?serverName=" + serverName;
+                return "http://localhost:8080/servers/server/config/get?serverName=" + serverName;
             }
         }
     };
